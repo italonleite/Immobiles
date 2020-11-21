eef51237fa46ad3079f65898daa24e7595e2026c
italo.leite <italo.leite@nibo.com.br>
2020-11-21 11:55:01 -0300

    deixando relacionando só em imóvel. imóvel tem um endereco

--- ProjetoImoveis/src/Immobiles.Domain/Models/Address.cs
+++ ProjetoImoveis/src/Immobiles.Domain/Models/Address.cs
@@ -12,9 +12,7 @@ namespace Immobiles.Domain.Models
         public string StreetAddress { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        public Immobile Immobiles { get; set; }
-
-
+        
 
         public Address()
         {
