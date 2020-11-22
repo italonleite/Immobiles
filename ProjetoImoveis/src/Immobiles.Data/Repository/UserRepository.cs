8b4acfdeec0eda72b777815c3c6990ff6bfc6637
italo.leite <italo.leite@nibo.com.br>
2020-11-22 17:12:41 -0300

    criando UserRepository

---
+++ ProjetoImoveis/src/Immobiles.Data/Repository/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using Immobiles.Data.Context;
+using Immobiles.Domain.Interface;
+using Immobiles.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Immobiles.Data.Repository
+{
+    public class UserRepository : Repository<User>, IUserRepository
+    {
+        public UserRepository(MyDbContext context) : base(context) { }
+
+        
+    }
+}
---
+++ ProjetoImoveis/src/Immobiles.Domain/Interface/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Immobiles.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Immobiles.Domain.Interface
+{
+    public interface IUserRepository : IRepository<User>
+    {
+        
+    }
+}
