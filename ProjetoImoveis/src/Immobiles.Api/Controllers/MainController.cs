4e2d846cda90adf8e7642ac42097935d0f747f20
italo.leite <italo.leite@nibo.com.br>
2020-11-22 17:23:07 -0300

    criando a controller base Main e Users

---
+++ ProjetoImoveis/src/Immobiles.Api/Controllers/MainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Immobiles.Api.Controllers
+{
+
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        //validação de notificacao de erro  
+        //validação de modelState
+        //validação da operação de negócio
+    }
+
+}
---
+++ ProjetoImoveis/src/Immobiles.Api/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Immobiles.Api.ViewModel;
+using Immobiles.Domain.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Immobiles.Api.Controllers
+{
+    [Route("api/usuarios")]
+    public class UsersController : MainController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UsersController(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ActionResult<IEnumerable<UserViewModel>>> GetAlls()
+        {
+            var user = _mapper.Map<IEnumerable<UserViewModel>>(await _userRepository.GetAll());
+            return Ok(user);
+        }
+    }
+
+
+}
