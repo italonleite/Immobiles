a69ca04465a1207a6c164bab19bca8a16ed712e3
italo.leite <italo.leite@nibo.com.br>
2020-11-22 17:31:24 -0300

    criando configuração para fazer um de para User vs UserViewModel com automapper

---
+++ ProjetoImoveis/src/Immobiles.Api/Configuration/AutomapperConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Immobiles.Api.ViewModel;
+using Immobiles.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Immobiles.Api.Configuration
+{
+    public class AutomapperConfig : Profile
+    {
+        public AutomapperConfig()
+        {
+            CreateMap<User, UserViewModel>().ReverseMap();
+        }
+    }
+}
